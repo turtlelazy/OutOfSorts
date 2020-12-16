3d9e8e62f739ad8c36e842cce2b3e38930bd27fd
turtlelazy <ishraqmahid@gmail.com>
2020-12-16 11:59:05 -0500

    created bubbleSort method untested

--- Sorts.java
+++ Sorts.java
@@ -1,3 +1,11 @@
 public class Sorts{
-    
+    public static void bubbleSort(int[] data){
+        for (int i = 0; i < data.length-1;i++){
+            if(data[i] > data[i+1]){
+                int temp = data[i];
+                data[i] = data[i+1];
+                data[i+1] = temp;
+            }
+        }
+    }
 }
