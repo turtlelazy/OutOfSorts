77494882b066039bd6b4966ee843cc4156ac5485
turtlelazy <ishraqmahid@gmail.com>
2020-12-18 12:07:30 -0500

    fixed bubble sorts

--- Sorts.java
+++ Sorts.java
@@ -1,10 +1,24 @@
 public class Sorts{
+
     public static void bubbleSort(int[] data){
-        for (int i = 0; i < data.length-1;i++){
-            if(data[i] > data[i+1]){
-                int temp = data[i];
-                data[i] = data[i+1];
-                data[i+1] = temp;
+        for(int a = 0; a<data.length;a++){
+            for (int i = 0; i < data.length-1;i++){
+                if(data[i] > data[i+1]){
+                    int temp = data[i];
+                    data[i] = data[i+1];
+                    data[i+1] = temp;
+                }
+            }
+    }
+    }
+
+    public static void selectionSort(int[] data) {
+        for(int i = 0; i<data.length;i++){
+            int smallest = data[i];
+            for(int a = i;i<data.length;a++){
+                if(data[a] < smallest){
+                    smallest = data[a];
+                }
             }
         }
     }
